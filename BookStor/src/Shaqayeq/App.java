9e4e43dfc3e407ce1169d3375859f4c563f181ce
ShaqayeqRahmani <andishe9.r@gmail.com>
2022-10-24 15:59:49 -0700

    Collection Assignme

---
+++ BookStor/src/Shaqayeq/App.java
@@ -0,0 +1,31 @@
+package Shaqayeq;
+import java.util.*;;
+
+public class App {
+   public static void main(String[] args) {
+    ArrayList<Integer> a = new ArrayList<Integer>();
+    a.add(1);
+    a.add(99);
+    a.add(18);
+ System.out.println(a);
+ a.remove(2);
+ for (int i = 0; i <= a.size() - 1; i++){
+    System.out.println(i + "" + a.get(i));
+ }
+  //Linkedlist
+ LinkedList<String> L = new LinkedList<>();
+ L.add("USE");
+ System.out.println("linkedlist"+ L);
+ L.remove(0);
+ for (int i = 0; i<= L.size() - 1; i++){
+     System.out.print(L.get(i) + " ");
+
+//vector
+Vector<Integer> vec = new Vector<>();
+for (int index = 0; index <= 10;i++){
+
+       }
+   }
+    
+}
+}
